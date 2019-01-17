e5f8b72991db4d97a255505e4273dead8222abc0
Jim Jeffery <jim.jeffery@gmail.com>
2019-01-17 18:31:21 +0000

    Add files via upload

---
+++ MessageIndex.swift
@@ -0,0 +1,75 @@
+//
+//  MessageIndex.swift
+//  FitDataProtocol
+//
+//  Created by Kevin Hoogheem on 2/4/18.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+import DataDecoder
+
+/// Message Index
+public struct MessageIndex {
+
+    /// Index is Selected
+    private(set) public var isSelected: Bool
+
+    /// Index
+    private(set) public var index: UInt16
+
+    /// Removed "internal" prefix to allow creation outside of Package.
+    init(value: UInt16) {
+        self.isSelected = (value & 0x8000) == 0x8000
+        self.index = (value & 0x0FFF)
+    }
+}
+
+internal extension MessageIndex {
+
+    internal func encode() -> Data {
+        var encode = Data()
+
+        let selected: UInt16 = isSelected == true ? 0x8000 : 0
+        let value = index | selected
+
+        encode.append(Data(from: value.littleEndian))
+
+        return encode
+    }
+
+    /// Decodes the Message Index
+    ///
+    /// - Parameters:
+    ///   - decoder: Decoder
+    ///   - endian: Endian
+    ///   - definition: Field Definition Message
+    ///   - data: Field Data Message
+    /// - Returns: Message Index
+    internal static func decode(decoder: inout DecodeData, endian: Endian, definition: FieldDefinition, data: FieldData) -> MessageIndex? {
+
+        let value = endian == .little ? decoder.decodeUInt16(data.fieldData).littleEndian : decoder.decodeUInt16(data.fieldData).bigEndian
+        
+        if value.isValidForBaseType(definition.baseType) {
+            return MessageIndex(value: value)
+        }
+
+        return nil
+    }
+}
